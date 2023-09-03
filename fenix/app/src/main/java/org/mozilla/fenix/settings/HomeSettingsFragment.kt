80c0108fda5ddd9e440f1e76b7722376e4fe5879
denizgencay <denizgencay35@gmail.com>
2023-09-03 17:10:16 +0300

    Unused customize homepage configuration fields removed from custom settings home page.

--- fenix/app/src/main/java/org/mozilla/fenix/settings/HomeSettingsFragment.kt
+++ fenix/app/src/main/java/org/mozilla/fenix/settings/HomeSettingsFragment.kt
@@ -6,15 +6,12 @@ package org.mozilla.fenix.settings
 
 import android.os.Bundle
 import androidx.navigation.findNavController
-import androidx.preference.CheckBoxPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import org.mozilla.fenix.FeatureFlags
 import org.mozilla.fenix.GleanMetrics.CustomizeHome
 import org.mozilla.fenix.R
-import org.mozilla.fenix.components.appstate.AppAction
-import org.mozilla.fenix.ext.components
 import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.ext.showToolbar
 import org.mozilla.fenix.utils.view.addToRadioGroup
@@ -43,22 +40,6 @@ class HomeSettingsFragment : PreferenceFragmentCompat() {
                         CustomizeHome.PreferenceToggledExtra(
                             newValue as Boolean,
                             "most_visited_sites",
-                        ),
-                    )
-
-                    return super.onPreferenceChange(preference, newValue)
-                }
-            }
-        }
-
-        requirePreference<CheckBoxPreference>(R.string.pref_key_enable_contile).apply {
-            isChecked = context.settings().showContileFeature
-            onPreferenceChangeListener = object : SharedPreferenceUpdater() {
-                override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
-                    CustomizeHome.preferenceToggled.record(
-                        CustomizeHome.PreferenceToggledExtra(
-                            newValue as Boolean,
-                            "contile",
                         ),
                     )
 
@@ -114,28 +95,6 @@ class HomeSettingsFragment : PreferenceFragmentCompat() {
                             "pocket",
                         ),
                     )
-
-                    return super.onPreferenceChange(preference, newValue)
-                }
-            }
-        }
-
-        requirePreference<CheckBoxPreference>(R.string.pref_key_pocket_sponsored_stories).apply {
-            isVisible = FeatureFlags.isPocketSponsoredStoriesFeatureEnabled(context)
-            isChecked = context.settings().showPocketSponsoredStories
-            onPreferenceChangeListener = object : SharedPreferenceUpdater() {
-                override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
-                    when (newValue) {
-                        true -> {
-                            context.components.core.pocketStoriesService.startPeriodicSponsoredStoriesRefresh()
-                        }
-                        false -> {
-                            context.components.core.pocketStoriesService.deleteProfile()
-                            context.components.appStore.dispatch(
-                                AppAction.PocketSponsoredStoriesChange(emptyList()),
-                            )
-                        }
-                    }
 
                     return super.onPreferenceChange(preference, newValue)
                 }
