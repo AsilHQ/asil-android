39bca14b54bc6ee66fd4ee78824d84809aa0968e
denizgencay <denizgencay35@gmail.com>
2023-11-17 22:59:12 +0300

    Redundant pin icon removed from home screen.

--- fenix/app/src/main/java/org/mozilla/fenix/home/topsites/TopSiteItemViewHolder.kt
+++ fenix/app/src/main/java/org/mozilla/fenix/home/topsites/TopSiteItemViewHolder.kt
@@ -138,8 +138,8 @@ class TopSiteItemViewHolder(
         binding.topSiteTitle.text = topSite.title
 
         if (topSite is TopSite.Pinned || topSite is TopSite.Default) {
-            val pinIndicator = getDrawable(itemView.context, R.drawable.ic_new_pin)
-            binding.topSiteTitle.setCompoundDrawablesWithIntrinsicBounds(pinIndicator, null, null, null)
+            //val pinIndicator = getDrawable(itemView.context, R.drawable.ic_new_pin)
+            binding.topSiteTitle.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
         } else {
             binding.topSiteTitle.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
         }
@@ -212,6 +212,7 @@ class TopSiteItemViewHolder(
     }
 
     companion object {
+        @SuppressLint("NonConstantResourceId")
         const val LAYOUT_ID = R.layout.top_site_item
     }
 }
