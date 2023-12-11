c5cda42c463b8eb0abbb4a19d5277b9b2082e0b9
denizgencay <denizgencay35@gmail.com>
2023-12-11 12:10:50 +0300

    Host block feature.

---
+++ fenix/app/src/main/java/org/mozilla/fenix/host_blocker/HostBlockerWorker.kt
@@ -0,0 +1,17 @@
+package org.mozilla.fenix.host_blocker
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class HostBlockerWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+    override fun doWork(): Result {
+        HostBlockerManager.fetchAndOverwriteHostFile(applicationContext)
+        SafeGazeBlockListManager.fetchAndOverwriteHostFile(applicationContext)
+        return Result.success()
+    }
+}
